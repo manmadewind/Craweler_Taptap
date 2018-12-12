20bc5ddd85ca7190fad56ffdf11a70405fd7cfbb
manmadewind <manmadewind@gmail.com>
2018-12-12 10:42:48 +0800

    1

--- get_latest.py
+++ get_latest.py
@@ -15,13 +15,25 @@ url_ref_list=[
     ('https://www.taptap.com/category/e24?page=2', u'游戏测试')
 ]
 
+c_chromedriver = "./driver/chromedriver_235.exe" #mac_245_chromedriver"
+
 
 def start():
-    chromedriver = "./chromedriver_235.exe"
-    driver = webdriver.Chrome(chromedriver)
+    driver = webdriver.Chrome(c_chromedriver)
     dm = DataManager()
 
-    df = pd.DataFrame(columns=['title', 'url', 'type', 'rating', 'ref', 'update_date', 'img'])
+    df = pd.DataFrame(columns=['title',  # 名称
+                               'url',    # 详情页面url
+                               'type',   # 游戏类型
+                               'rating', # 评分
+                               'intro',  # 简介
+                               'factory',# 厂商
+                               'star',   # 关注人数
+                               'update', # 更新时间
+                               'ref',
+                               'update_date', # 爬虫抓到内容的时间
+                               'img'])
+    
     for url,ref in url_ref_list:
         print('dealing with %s' % url)
         driver.get(url)
@@ -50,6 +62,12 @@ def scroll_untill_bottom_stepbystep(driver, steps=3):
     print("BOTTOM NOW:)")
 
 
+def _get_firstText_or_empty(elements):
+    if len(elements)>0:
+        return elements[0].text.strip()
+    return ""
+
+
 class DataManager():
     def __init__(self):
         self.df_history=pd.read_csv('./new_game_history.csv', encoding='utf_8_sig')
@@ -61,6 +79,8 @@ class DataManager():
         df = pd.DataFrame(columns=['title', 'url', 'type', 'rating', 'ref', 'update_date', 'img'])
         items = driver.find_elements_by_class_name('taptap-app-item')
         date_str = datetime.datetime.now().strftime('%Y%m%d')
+        detail_driver = webdriver.Chrome(c_chromedriver)
+
         for item in items:
             e_href = item.find_elements_by_class_name('app-item-image')
             href   = e_href[0].get_attribute('href').strip() if e_href != [] else ''
@@ -75,12 +95,49 @@ class DataManager():
             e_gametype = item.find_elements_by_css_selector('div > span > a')
             gametype   = e_gametype[0].text.strip() if e_gametype != [] else ''
 
+            # 基础数据
+            basic_dict = {
+                'title':title,
+                'url':href,
+                'img': imgsrc,
+                'type':gametype,
+                'rating':rating,
+                'ref':ref,
+                'update_date':date_str # 爬虫爬到的时间
+            }
+            # 详情页数据获取
+            detail_dict = self.get_detail(detail_driver, href)
+
+            # 合并成为一行的数据
+            row_dict = dict(basic_dict.items() | detail_dict.items())
+
             if title not in set(self.df_history.title):
-                row = pd.Series([title, href, imgsrc, gametype, rating, ref, date_str],
-                          index=['title', 'url', 'img', 'type', 'rating','ref', 'update_date'])
-                self.df_history=self.df_history.append(row, ignore_index=True) # append to history
-                df=df.append(row, ignore_index=True) # add to new
+                # row = pd.Series([title, href, imgsrc, gametype, rating, ref, date_str],
+                #           index=['title', 'url', 'img', 'type', 'rating','ref', 'update_date'])
+                # self.df_history=self.df_history.append(row, ignore_index=True) # append to history
+                self.df_history=self.df_history.append(row_dict, ignore_index=True) # append to history
+                df = df.append(row_dict, ignore_index=True) # add to new
         return df
+
+
+    def get_detail(self, driver, url):
+        driver.get(url)
+        print("ing "+url)
+
+        star_selector = 'body > div.container.app-main-container > div > div > section.app-show-main.taptap-page-main > div.show-main-header > div.main-header-text > div.header-text-download > div.text-download-text > p > span'
+        factory_selector ='body > div.container.app-main-container > div > div > section.app-show-main.taptap-page-main > div.show-main-body.collapse.in.first > div.main-body-info > ul > li:nth-child(4) > a'
+        intro_xpath   = '//*[@id="description"]'
+        update_selector = 'body > div.container.app-main-container > div > div > section.app-show-main.taptap-page-main > div.show-main-body.collapse.in.first > div.main-body-info > ul > li:nth-child(3) > span.info-item-content'
+
+
+        intro   = _get_firstText_or_empty(driver.find_elements_by_xpath(intro_xpath))
+        factory = _get_firstText_or_empty(driver.find_elements_by_css_selector(factory_selector))
+        star    = _get_firstText_or_empty(driver.find_elements_by_class_name("count-stats"))
+        update  = _get_firstText_or_empty(driver.find_elements_by_css_selector(update_selector))
+
+        print(url, intro, star, update)
+        return {'intro':intro, 'factory':factory, 'star':star, 'update': update}
+
 
 if __name__ == '__main__':
     start()
